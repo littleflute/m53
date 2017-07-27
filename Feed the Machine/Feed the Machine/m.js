7b591a7d535b95198bae140702f06bda5e1c2a68
littleflute <13699175164@sina.cn>
2017-07-27 17:03:45 -0500

    Add files via upload

---
+++ Feed the Machine/Feed the Machine/m.js
@@ -0,0 +1,13 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "02 Slave State.mp3";		
+f[2]	= "04 Kill the Beautiful Ones.mp3";		
+f[3]	= "05 Under the Texas Sun.mp3";		
+f[4]	= "06 Systematic Entrapment.mp3";		
+f[5]	= "07 White Collar Blueslack.mp3";		
+f[6]	= "08 Dean Dirt Is Dead.mp3";		
+f[7]	= "09 Another Plight.mp3";		
+f[8]	= "10 Feed the Machine.mp3";		
+f[9]	= "11 Disease Called Man.mp3";		
+f[10]	= "1492.mp3";		
+f[11]	= "Attica.mp3";		

9a586d2c9cca8bfa622325f2765a47d41d78dd70
littleflute <13699175164@sina.cn>
2017-07-27 16:50:58 -0500

    Add files via upload

---
+++ Nicola Benedetti/Tchaikovsky, Bruch- Violin Concertos/m.js
@@ -0,0 +1,8 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Tchaikovsky - Violin Concerto in D major, op. 35 - I. Allegro moderato.mp3";		
+f[2]	= "02 II. Canzonetta Andante.mp3";		
+f[3]	= "03 III. Finale Allegro vivacissimo.mp3";		
+f[4]	= "04 Bruch - Violin Concerto No.1. in G minor, op. 26 - I. Vorspiel- Allegro moderato.mp3";		
+f[5]	= "05 II. Adagio.mp3";		
+f[6]	= "06 III. Finale Allegro energico.mp3";		
